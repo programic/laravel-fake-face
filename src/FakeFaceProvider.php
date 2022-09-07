b2248032d552ce4392b876f836ebddc10d3144c3
Jeffrey van den Hondel <j.vandenhondel@programic.com>
2022-09-07 09:06:34 +0200

    [use-faker-config] chore: use faker config

--- src/FakeFaceProvider.php
+++ src/FakeFaceProvider.php
@@ -11,8 +11,7 @@ class FakeFaceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(Generator::class, function () {
-            info(Config::get('app.faker_locale'));
-            $faker = Factory::create(Config::get('app.faker_locale'));
+            $faker = Factory::create($this->app['config']->get('app.faker_locale'));
             $faker->addProvider(new FakeFaceService());
             return $faker;
         });
